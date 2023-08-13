d7500fa24dfb9b98ee75563355af7889127f662e
EDDY <eddmunyiri@gmail.com>
2023-08-13 18:44:42 +0300

    update file

--- Rock_paper_scissors.py
+++ Rock_paper_scissors.py
@@ -3,8 +3,15 @@
 import random 
 
 def get_choices():
-  player_choice = input("On three, choose one: Rock,Paper,Scissors...  ")
   options = ["Rock", "Paper", "Scissors"]
+
+  while True:
+    player_choice = input("Rock,Paper,Scissors...  ").capitalize()
+    if player_choice in options:
+      break
+    else:
+      print("Invalid Input! Choose from: Rock, Paper or Scissors")
+
   computer_choice = random.choice(options)
   # using a dictionary to store key data values in key value pairs
   choices = {"player": player_choice, "computer": computer_choice}
@@ -13,12 +20,8 @@ def get_choices():
 
 def check_win(player, computer):
   print(f"You chose {player}, and Computer chose {computer}")
-#using nested if statements to:
-#1)Allow to check more than one condition.
-#2)Return different values depending on the reults and
-#3) make the code to be more understable
   if player == computer:
-    return "What a tie!"
+    return "It's a tie!"
   elif player == "Rock":
     if computer == "Scissors":
       return "Rock smashes Scissors, You win! bloody hell!"
@@ -35,8 +38,17 @@ def check_win(player, computer):
     else:
       return "Rock smashes Scissors, You Lose!, HA!HA!HA!"
 
-choices = get_choices() #This will access the dictionary values in the choices variable that is inside the get_Choices functiion.
-result = check_win(choices["player"], choices["computer"])
+while True:
+  choices = get_choices() #This will access the dictionary values in the choices variable that is inside the get_Choices functiion.
+  result = check_win(choices["player"], choices["computer"])
 # Using brackets to access the value of the desired key. In the above case, the value of both the player and computer keys. 
-print(result)
+  print(result)
+
+  play_again = input("would you like to play again?").lower()
+
+  if play_again != "yes":
+    print("Bye!")
+    break
+
+
   
